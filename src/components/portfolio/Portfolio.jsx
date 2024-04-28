0166afb49746130d1fdb0c4f290ad6bebdeb7583
garen <karmartir@gmail.com>
2024-04-27 21:17:17 -0400

    add functionality to button on portfolio.jsx to open links

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -8,24 +8,28 @@ const items = [
     title: "Project 1",
     img: "./project_1.png",
     desc: "Internet store project",
+    url: "https://neon-pika-975659.netlify.app/" // Demo URL for Project 1
   },
   {
     id: 2,
     title: "Project 2",
-    img: "./project_2.png",
-    desc: "Internet store project",
+    img: "./project2-hi-res.png",
+    desc: "Design example made with love",
+    url: "https://www.behance.net/gallery/161746913/Project-1" // Demo URL for Project 2
   },
   {
     id: 3,
     title: "Project 3",
-    img: "./project_3.png",
-    desc: "Internet store project",
+    img: "./project3-hi-res.png",
+    desc: "The chat messenger",
+    url: "https://www.behance.net/gallery/162681601/CHATapp-concept-design" // Demo URL for Project 3
   },
   {
     id: 4,
     title: "Project 4",
-    img: "./project_4.png",
-    desc: "Internet store project",
+    img: "./project_4h.png",
+    desc: "BearBrick Black VIP -PoSter design concept",
+    url:"https://www.behance.net/gallery/162482541/Sample"
   },
 ];
 
@@ -34,13 +38,20 @@ const Single = ({ item }) => {
 
   const { scrollYProgress } = useScroll({
     target: ref,
-    // offset: ['start start', 'end start']
   });
 
   const y = useTransform(scrollYProgress, [0, 1], [-150, 150]);
 
+  const handleDemoClick = () => {
+    if (item.url) {
+      window.open(item.url, "_blank"); // Open the URL in a new tab
+    } else {
+      console.log("No demo URL provided for this project.");
+    }
+  };
+
   return (
-    <section >
+    <section>
       <div className="container">
         <div className="wrapper">
           <div className="imgContainer" ref={ref}>
@@ -50,7 +61,7 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{ y }}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            {item.url && <button onClick={handleDemoClick}>See project</button>}
           </motion.div>
         </div>
       </div>
@@ -75,10 +86,7 @@ const Portfolio = () => {
     <div className="portfolio" ref={ref}>
       <div className="progress">
         <h1>Featured Works</h1>
-        <motion.div
-          style={{ scaleX: scaleX }}
-          className="progressBar"
-        ></motion.div>
+        <motion.div style={{ scaleX: scaleX }} className="progressBar"></motion.div>
       </div>
       {items.map((item) => (
         <Single item={item} key={item.id} />
